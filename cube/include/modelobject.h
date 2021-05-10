54e9a5238e1deb7ba87915b414536e6a007b92eb
SzalmasBalazs <78908375+SzalmasBalazs@users.noreply.github.com>
2021-05-10 18:52:23 +0200

    Update modelobject.h

--- cube/include/modelobject.h
+++ cube/include/modelobject.h
@@ -3,14 +3,17 @@
 
 #include <obj/model.h>
 #include "utils.h"
+#include "texture.h"
 
 typedef struct Ship
 {
 	Model player_ship;
+	Material material;
 	float model_scale;
 	vec3 position;
     vec3 speed;
 	vec3 rotation;
+	vec3 rot_speed; 
 }Ship;
 
 /*draws the player ship.*/
@@ -19,8 +22,10 @@ void draw_ship(Ship* ship);
 /*setup operations*/
 void init_ship(Ship* ship);
 
+/*function responsible for moving the ship on its y axis*/
 void set_ship_side_speed(Ship* ship,double speed);
 
+/*function responsible for moving the ship on its z axis*/
 void set_ship_horizontal_speed(Ship* ship,double speed);
 
 void update_ship(Ship* ship,double time);
